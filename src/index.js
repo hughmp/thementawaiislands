d805132f49b24c3363d3748cdc67bf353bbe050d
hughmp <hughjmp@gmail.com>
2017-04-30 16:47:09 +0100

    add google analytic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import 'babel-polyfill';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-47185042-1');
 
 import Home from './components/Home';
 import About from './components/About';
@@ -15,8 +18,17 @@ import Logistics from './components/Logistics';
 import Reservations from './components/Reservations';
 import Press from './components/Press';
 
+const onUpdate = () => {
+  // scroll back to the top
+  window.scrollTo(0, 0);
+
+  // log page view with google analytics
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
 ReactDOM.render((
-  <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
+  <Router history={browserHistory} onUpdate={onUpdate}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='/brochure' component={About} />
