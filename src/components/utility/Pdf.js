fd96e87f2c0750297489b711a39c783d4fff30f2
hughmp <hughjmp@gmail.com>
2018-02-11 18:33:49 +0000

    update brochure

--- src/components/utility/Pdf.js
+++ src/components/utility/Pdf.js
@@ -4,7 +4,7 @@ import './styles/pdf.css';
 
 const Pdf = () => (
   <div className='pdf-wrapper'>
-    <iframe src="https://cdn.flipsnack.com/widget/flipsnackwidget.html?hash=fdn34j3cg&bgcolor=EEEEEE&t=1493578279" seamless="seamless" scrolling="no" frameBorder="0" allowTransparency="true" allowFullScreen="true" />
+    <iframe src="https://www.yumpu.com/en/embed/view/o4pbQjIAnaKS6GMd" seamless="seamless" scrolling="no" frameBorder="0" allowTransparency="true" allowFullScreen="true" />
   </div>
 );
 
